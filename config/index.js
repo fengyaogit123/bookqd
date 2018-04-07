84775ef7c5d631d771d3e233b8a8f0888b5c4228
fengyao123 <1724238345@qq.com>
2018-04-07 11:38:33 +0800

    '修改端口'

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
         },
 
         // Various Dev Server settings
-        host: 'localhost', // can be overwritten by process.env.HOST
+        host: '127.0.0.1', // can be overwritten by process.env.HOST
         port: 9898, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: true,
         errorOverlay: true,
